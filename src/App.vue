119233e4a8e9260187e402351234319868a508de
Quocvuong10299 <lequocvuongct.2016@gmail.com>
2020-02-21 10:38:53 +0700

    change component name

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld/>
+    <ToDoApp/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import ToDoApp from './components/ToDoApp'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ToDoApp
   }
 }
 </script>
